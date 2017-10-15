d58537b5d3782f802c8daa962b75d34d3c0418bc
vutienduong <vu.dduong@eemploymenthero.com>
2017-10-15 10:29:09 +0700

    Update ajax for adding and removing dish in javascript and in users_controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012083607) do
+ActiveRecord::Schema.define(version: 20171014111650) do
 
   create_table "dish_orders", force: :cascade do |t|
     t.integer  "dish_id"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 20171012083607) do
   end
 
   create_table "menus", force: :cascade do |t|
-    t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
   end
 
   create_table "orders", force: :cascade do |t|
