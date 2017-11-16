debcd99934e01da55f07bffc63fd3401296809a8
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-16 14:22:42 +0700

    change avatar to image_logo

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,5 +10,5 @@ class Restaurant < ActiveRecord::Base
   }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image_logo, :content_type => /\Aimage\/.*\Z/
 end
