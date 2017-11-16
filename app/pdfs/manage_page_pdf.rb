938dc087d67e9f255566e89ccafe40d8e1e1d24f
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-17 04:53:45 +0700

    change display od PDF file

--- app/pdfs/manage_page_pdf.rb
+++ app/pdfs/manage_page_pdf.rb
@@ -17,22 +17,43 @@ class ManagePagePdf < Prawn::Document
     table_content
   end
 
+  def text_content
+    y_position = cursor
+    bounding_box([0, y_position], :width => 270, :height => 100) do
+      text '', size: 15, style: :bold
+    end
+  end
+
+  def date_title
+    text Date.today.to_s, size: 15, style: :bold
+  end
+
   def table_content
     # This makes a call to product_rows and gets back an array of data that will populate the columns and rows of a table
     # I then included some styling to include a header and make its text bold. I made the row background colors alternate between grey and white
     # Then I set the table column widths
+
+    date_title
+
+    text "List Orders", size: 15, style: :bold
 
     table rows_orders do
       row(0).font_style = :bold
       self.header = true
       self.row_colors = ['DDDDDD', 'FFFFFF']
     end
+    text_content
+
+    text "List Dishes", size: 15, style: :bold
 
     table rows_dishes do
       row(0).font_style = :bold
       self.header = true
       self.row_colors = ['DDDDDD', 'FFFFFF']
     end
+    text_content
+
+    text "List Costs", size: 15, style: :bold
 
     table rows_costs do
       row(0).font_style = :bold
@@ -78,9 +99,9 @@ class ManagePagePdf < Prawn::Document
       restaurant = e.first
       details = e.last
       res_row = [restaurant.name.to_s,
-           restaurant.phone.to_s,
-           details[:cost].to_s,
-           '']
+                 restaurant.phone.to_s,
+                 details[:cost].to_s,
+                 '']
 
       dish_rows = details[:dishes].map do |dish, cost|
         ['', dish.name.to_s,
@@ -94,7 +115,7 @@ class ManagePagePdf < Prawn::Document
       idx += 1
       rs += dish_rows
     end +
-    [['', '', 'TOTAL COST', @presenter[:total_cost].to_s]]  +
-    [['', '', 'BUDGET', @presenter[:budget].to_s]]
+        [['', '', 'TOTAL COST', @presenter[:total_cost].to_s]] +
+        [['', '', 'BUDGET', @presenter[:budget].to_s]]
   end
 end
