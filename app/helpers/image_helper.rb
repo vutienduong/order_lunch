d93f873c0f0a9373ef138cfaee94ecd0922fe81d
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-16 17:10:15 +0700

    - Change image displaying in erb files

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -3,7 +3,7 @@ module ImageHelper
     image.nil? ? image_path("default/default_#{object}") :"data:image/jpg;base64,#{Base64.encode64(image)}"
   end
 
-  def get_s3_image_source restaurant
-    restaurant.image_logo.url(:square)
+  def get_s3_image_source restaurant, image_size = 'square'
+    restaurant.blank? ? '' : restaurant.image_logo.url(image_size.to_sym)
   end
 end
