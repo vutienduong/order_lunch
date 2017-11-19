cc610979f58b8209abeb1ba4121c3c015598c743
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-19 12:17:24 +0700

    - Add 'read more' by JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,31 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
+$(function(){ /* to make sure the script runs after page load */
+
+    $('.item-long-content').each(function(event){ /* select all divs with the item class */
+
+        var max_length = 80; /* set the max content length before a read more link will be added */
+
+        if($(this).html().length > max_length){ /* check for content length */
+
+            var short_content 	= $(this).html().substr(0,max_length); /* split the content in two parts */
+            var long_content	= $(this).html().substr(max_length);
+
+            $(this).html(short_content+
+                '<a href="#" class="read_more"><br/>Read More</a>'+
+                '<span class="more_text" style="display:none;">'+long_content+'</span>'); /* Alter the html to allow the read more functionality */
+
+            $(this).find('a.read_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+                event.preventDefault(); /* prevent the a from changing the url */
+                $(this).hide(); /* hide the read more button */
+                $(this).parents('.item-long-content').find('.more_text').show(); /* show the .more_text span */
+            });
+        }
+    });
+});
+
+
 function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
