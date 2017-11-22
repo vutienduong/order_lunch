e1c5cf646798113069a0622626059f68c27b69a7
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-22 18:56:17 +0700

    - Change from 2 tabs to 1 tabs, support filter 'restaurants' and 'tags'

--- db/migrate/20171122050409_create_tag_table.rb
+++ db/migrate/20171122050409_create_tag_table.rb
@@ -2,7 +2,6 @@ class CreateTagTable < ActiveRecord::Migration
   def change
     create_table :tags do |t|
       t.string :name
-      t.references :dishes, index: true, foreign_key: true
     end
   end
 end
