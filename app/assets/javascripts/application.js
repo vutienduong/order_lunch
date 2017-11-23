e16e779b590394e1bbc598587b3f00bf4f9619f3
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-23 17:24:22 +0700

    Revert "- Soft dish follow Restaurant-> Tags"
    This reverts commit daa2f4d586ff21d6baa555730bb4937d1518c9e0.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,31 +15,25 @@
 //= require_tree .
 //= require bootstrap-sprockets
 
-$(function () { /* to make sure the script runs after page load */
+$(function(){ /* to make sure the script runs after page load */
 
-    $('.item-long-content').each(function (event) { /* select all divs with the item class */
+    $('.item-long-content').each(function(event){ /* select all divs with the item class */
 
-        var max_length = 80;
-        /* set the max content length before a read more link will be added */
+        var max_length = 80; /* set the max content length before a read more link will be added */
 
-        if ($(this).html().length > max_length) { /* check for content length */
+        if($(this).html().length > max_length){ /* check for content length */
 
-            var short_content = $(this).html().substr(0, max_length);
-            /* split the content in two parts */
-            var long_content = $(this).html().substr(max_length);
+            var short_content 	= $(this).html().substr(0,max_length); /* split the content in two parts */
+            var long_content	= $(this).html().substr(max_length);
 
-            $(this).html(short_content +
-                '<a href="#" class="read_more"><br/>Read More</a>' +
-                '<span class="more_text" style="display:none;">' + long_content + '</span>');
-            /* Alter the html to allow the read more functionality */
+            $(this).html(short_content+
+                '<a href="#" class="read_more"><br/>Read More</a>'+
+                '<span class="more_text" style="display:none;">'+long_content+'</span>'); /* Alter the html to allow the read more functionality */
 
-            $(this).find('a.read_more').click(function (event) { /* find the a.read_more element within the new html and bind the following code to it */
-                event.preventDefault();
-                /* prevent the a from changing the url */
-                $(this).hide();
-                /* hide the read more button */
-                $(this).parents('.item-long-content').find('.more_text').show();
-                /* show the .more_text span */
+            $(this).find('a.read_more').click(function(event){ /* find the a.read_more element within the new html and bind the following code to it */
+                event.preventDefault(); /* prevent the a from changing the url */
+                $(this).hide(); /* hide the read more button */
+                $(this).parents('.item-long-content').find('.more_text').show(); /* show the .more_text span */
             });
         }
     });
@@ -83,30 +77,6 @@ function showDishesForRetaurant(evt, restaurantName) {
     //document.getElementById(restaurantName).style.display = "block";
     restaurantName.style.display = "block";
     evt.currentTarget.className += " active";
-
-    setCurrentButtonSameAsSelectedName(evt)
-}
-
-function setCurrentButtonSameAsSelectedName(evt) {
-    try {
-        self = $(evt.srcElement)
-        caret = "  <span class=\"caret\"></span>"
-        if (self.attr('id') == undefined) {
-            $('#restautant-dropdown-button').html('[Restaurant] ' + self.text() + caret)
-            $('#tag-dropdown-button').html("All tags" + caret)
-        }
-        else if (self.attr('id').includes('tag_tab_link')) {
-            $('#tag-dropdown-button').html('[Tag] ' + self.text() + caret)
-        }
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
-
-function hiddenOtherTags(restaurantName) {
-    $('a[id^="tag_tab_link_"]').hide()
-    $("a[id^=\"" + restaurantName + "\"]").show()
 }
 
 function showPrice(dish) {
