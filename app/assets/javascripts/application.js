6b57f9d2853a6e1f3d695bc9c6f3ac139c30ad32
vutienduong <vu.dduong@eemploymenthero.com>
2017-11-23 17:44:05 +0700

    Update application.js - comment out 1 new functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -87,7 +87,6 @@ function showDishesForRetaurant(evt, restaurantName) {
     setCurrentButtonSameAsSelectedName(evt)
 }
 
-/*
 function setCurrentButtonSameAsSelectedName(evt) {
     try {
         self = $(evt.srcElement)
@@ -105,11 +104,10 @@ function setCurrentButtonSameAsSelectedName(evt) {
     }
 }
 
-function hiddenOtherTags(restaurantName) {
+/*function hiddenOtherTags(restaurantName) {
     $('a[id^="tag_tab_link_"]').hide()
     $("a[id^=\"" + restaurantName + "\"]").show()
-}
-*/
+}*/
 
 function showPrice(dish) {
     alert(dish)
