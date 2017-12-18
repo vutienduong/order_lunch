08206124568fd3ec363392ff071e316038d16f62
vutienduong <vu.dduong@eemploymenthero.com>
2017-12-18 08:41:05 +0700

    - Add pagination

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,14 @@
 class MenusController < ApplicationController
   before_action :require_login
+  include ApplicationHelper
+
   def index
-    @menus = Menu.all.order(date: :desc)
+    @page = page_param[:page].to_i
+    @page = 1 if @page.blank? || @page.zero?
+    @menus = Menu.order(date: :desc)
+                 .limit(NUMBER_OF_MENU_PER_PAGE)
+                 .offset((@page - 1) * NUMBER_OF_MENU_PER_PAGE)
+    @num_of_pages = (Menu.count/NUMBER_OF_MENU_PER_PAGE.to_f).ceil
   end
 
   def show
@@ -11,5 +18,9 @@ class MenusController < ApplicationController
   private
   def menu_params
     params.require(:menu).permit(:date, :restaurant_ids => [])
+  end
+
+  def page_param
+    params.permit(:page)
   end
 end
