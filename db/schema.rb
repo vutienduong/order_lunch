326e1a68f4f92cd2a8206592e0c7ba71c5b61378
vutienduong <vu.dduong@eemploymenthero.com>
2018-05-15 08:41:03 +0700

    Revert version for migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514230028) do
+ActiveRecord::Schema.define(version: 20171130015214) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
