71ae3da0910d356ae414f314a9c584db600ed485
vutienduong <vu.dduong@eemploymenthero.com>
2018-05-15 08:41:34 +0700

    revert older version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130015214) do
+ActiveRecord::Schema.define(version: 20171122071743) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "title"
