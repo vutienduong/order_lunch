55672b7ce00533d0d722f0f6ed876b250e09b2f0
anhvy <vy.nguyen@employmenthero.com>
2019-05-07 16:21:01 +0700

    [WIP] Updating UI for legacy order

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,31 +3,31 @@ class DashboardsController < ApplicationController
   before_action :require_login
 
   def index
-    # select_date = select_date_params[:select_date]
-    # select_date = select_date ? Date.parse(select_date) : Date.today
+    @select_date = Date.parse(params[:select_date].presence || Date.today.to_s)
+    @menu = Menu.where('DATE(date)=?', @select_date).first
 
-    # @menu = Menu.where('DATE(date)=?', select_date).first
-    # @select_date = select_date
+    return if @menu.blank?
+    if @select_date < Date.today ||
+        (@select_date == Date.today && @menu.is_lock? && Time.current > @menu.locked_at)
+      @locked = true
+      @locked_time = @menu.locked_at
+      return
+    end
 
-    # return if @menu.blank?
-    # if select_date < Date.today ||
-    #     (select_date == Date.today && @menu.is_lock? && Time.current > @menu.locked_at)
-    #   @locked = true
-    #   @locked_time = @menu.locked_at
-    #   return
-    # end
+    @today_order = Orders::RetrieveService.find_order_by_user_id_and_date session[:user_id], @select_date
 
-    # @today_order = Orders::RetrieveService.find_order_by_user_id_and_date session[:user_id], select_date
+    @r_tags = Menus::RetrieveService.new(@menu).collect_follow_tags_for_each_restaurant
 
-    # @r_tags = Menus::RetrieveService.new(@menu).collect_follow_tags_for_each_restaurant
+    @total_price = @today_order.blank? ? 0 : @today_order.cal_total_price
 
-    # @total_price = @today_order.blank? ? 0 : @today_order.cal_total_price
+    @all_orders = Order.where('DATE(date)=?', @select_date)
 
-    # @all_orders = Order.where('DATE(date)=?', select_date)
-
-    # @available_restaurants = @menu.available_restaurants(Time.current)
-    # @avg_cost_on_month = AverageCostService.call(current_user.id)
-    # num_day_of_month = ParseDateService.weekdays_in_month_given_a_date(Date.current)
-    # @total_allow_budget_for_month = num_day_of_month * Order::MONTH_AVG_LIMIT
+    @available_restaurants = @menu.available_restaurants(Time.current)
+    @avg_cost_on_month = AverageCostService.call(current_user.id)
+    num_day_of_month = ParseDateService.weekdays_in_month_given_a_date(Date.current)
+    @total_allow_budget_for_month = num_day_of_month * Order::MONTH_AVG_LIMIT
+    @api_requests = {}
   end
+
+  private
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   layout 'sessions'
 
   def new
-    redirect_to user_path(current_user) if logged_in?
+    redirect_to root_path if logged_in?
   end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
       log_in @user
-      redirect_back_or @user
+      redirect_back_or root_path
     else
       flash[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
