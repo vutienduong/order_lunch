942b069ab8a4c0da5da8b0ef9f982d717f2df067
anhvy <vy.nguyen@employmenthero.com>
2019-05-20 15:33:20 +0700

    Finished UI for current order

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -14,20 +14,8 @@ class DashboardsController < ApplicationController
       return
     end
 
-    @today_order = Orders::RetrieveService.find_order_by_user_id_and_date session[:user_id], @select_date
-
     @r_tags = Menus::RetrieveService.new(@menu).collect_follow_tags_for_each_restaurant
-
-    @total_price = @today_order.blank? ? 0 : @today_order.cal_total_price
-
     @all_orders = Order.where('DATE(date)=?', @select_date)
-
     @available_restaurants = @menu.available_restaurants(Time.current)
-    @avg_cost_on_month = AverageCostService.call(current_user.id)
-    num_day_of_month = ParseDateService.weekdays_in_month_given_a_date(Date.current)
-    @total_allow_budget_for_month = num_day_of_month * Order::MONTH_AVG_LIMIT
-    @api_requests = {}
   end
-
-  private
 end
